4eb05be8d6c4ff6fd0c96746a902dd01218453d5
griffegg <gggriffes@gmail.com>
2015-10-18 22:57:32 +0000

    working

--- sense_hat_client.py
+++ sense_hat_client.py
@@ -8,7 +8,9 @@ Send sense hat data to UDP server inside the house
 # Libraries
 ###################################################################
 import sys, os, socket, time
+import webcolors
 from sense_hat import SenseHat
+import RPi.GPIO as GPIO
 
 #########################
 # Local libraries
@@ -40,11 +42,18 @@ class UDPClient(object):
         try:
             if len(message):
                 message = str(message)
-                print "Sending Message:", message
+#                print "Sending Message:", message
                 self.UDPSock.sendto(message, self.addr)
         except:
             self.UDPSock.close()
             sys.exit(0)
+
+# function for celcius to farenheiht conversion
+def c2f (centigrade):
+    """
+    Convert Centigrade to Fahrenheit
+    """
+    return 9.0*centigrade/5.0 + 32
 
 ###############################
 #
@@ -56,26 +65,57 @@ if __name__ == '__main__':  #test code
     DEBUG = True
     sense = SenseHat()
     sense.show_message("Hello")
-
+    sense.set_imu_config(True, True, True)  # compass only
+    
     UDP_client = UDPClient(33333)
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(20, GPIO.IN)
+
+    sense.set_rotation(0)
+    red = webcolors.name_to_rgb("red")
+    yellow = webcolors.name_to_rgb("yellow")
+    green = webcolors.name_to_rgb("green")
+    white = webcolors.name_to_rgb("white")
+    blue = webcolors.name_to_rgb("blue")
+    gold = webcolors.name_to_rgb("gold")
 
     #############################
     # Main while loop
     #############################
     try:
-        event_change = False
+        event_change = True
         return_string = "null event"
         
         while True:                 # The main loop
 
-            sense.show_message("Serve")
+            return_string = "valve=pool"
 
-            if (event_change):
-                print(return_string)
-                UDP_client.sendMessage(return_string)
-                event_change = False
+            if (not GPIO.input(20)):
+                return_string = "valve=spa"
+                sense.show_message("SPA!", text_colour=yellow, back_colour=red)                
+                time.sleep(1)
+                sense.clear()
+            else:
+                sense.clear()
                 
-            time.sleep(3)       
+            humidity = sense.get_humidity()
+            return_string += (",humid=%0.0f" % humidity)
+
+            tempc = sense.get_temperature()
+            tempf = c2f(tempc)
+            return_string += (",tempe=%0.0f" % tempf)
+
+            pressure = sense.get_pressure()
+            return_string += (",press=%0.0f" % pressure)
+
+            orientation = sense.get_orientation_degrees()
+            return_string += (",pitch={pitch},roll={roll},yaw={yaw}".format(**orientation))
+
+            print(return_string)
+            UDP_client.sendMessage(return_string)
+
+            time.sleep(1)
 
     #############################
     # End main while loop
@@ -83,7 +123,9 @@ if __name__ == '__main__':  #test code
 
     except KeyboardInterrupt:
         print 'Keyboard Interrupt Exception!'
+        sense.clear()
 
     except IOError:
         print 'I/O Error Exception! Quitting'
+        sense.clear()
 
