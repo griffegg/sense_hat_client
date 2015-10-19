bb3cc8c8673fc0350ea62a3831a2826a83768408
griffegg <gggriffes@gmail.com>
2015-10-19 12:45:56 +0000

    first deployment

--- sense_hat_client.py
+++ sense_hat_client.py
@@ -2,6 +2,11 @@
 """
 Created 10/17/15 by Greg Griffes
 Send sense hat data to UDP server inside the house
+
+to run at startup:
+sudo nano /etc/rc.local
+add the line before the exit:
+/home/pi/projects/sense_hat_client.sh &
 """
 
 ###################################################################
@@ -9,8 +14,9 @@ Send sense hat data to UDP server inside the house
 ###################################################################
 import sys, os, socket, time
 import webcolors
-from sense_hat import SenseHat
+#from sense_hat import SenseHat
 import RPi.GPIO as GPIO
+#import pygame
 
 #########################
 # Local libraries
@@ -63,59 +69,70 @@ def c2f (centigrade):
 if __name__ == '__main__':  #test code
 
     DEBUG = True
-    sense = SenseHat()
-    sense.show_message("Hello")
-    sense.set_imu_config(True, True, True)  # compass only
+#    sense = SenseHat()
+#    sense.show_message("Hello")
+#    sense.set_imu_config(True, True, True)  # compass only
     
     UDP_client = UDPClient(33333)
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(20, GPIO.IN)
 
-    sense.set_rotation(0)
-    red = webcolors.name_to_rgb("red")
-    yellow = webcolors.name_to_rgb("yellow")
-    green = webcolors.name_to_rgb("green")
-    white = webcolors.name_to_rgb("white")
-    blue = webcolors.name_to_rgb("blue")
-    gold = webcolors.name_to_rgb("gold")
+##    host_name = socket.gethostname()
+##    host_ip = socket.gethostbyname(host_name)
+##    print host_ip
+
+##    sense.set_rotation(0)
+##    red = webcolors.name_to_rgb("red")
+##    yellow = webcolors.name_to_rgb("yellow")
+##    green = webcolors.name_to_rgb("green")
+##    white = webcolors.name_to_rgb("white")
+##    blue = webcolors.name_to_rgb("blue")
+##    gold = webcolors.name_to_rgb("gold")
 
     #############################
     # Main while loop
     #############################
     try:
-        event_change = True
+        done = False
         return_string = "null event"
+
+        print "pool and spa client running..."
         
-        while True:                 # The main loop
+        while not done:                 # The main loop
+
+##            for event in pygame.event.get():
+##                if event.type == KEYDOWN:
+##                    done = True
 
             return_string = "valve=pool"
 
             if (not GPIO.input(20)):
                 return_string = "valve=spa"
-                sense.show_message("SPA!", text_colour=yellow, back_colour=red)                
-                time.sleep(1)
-                sense.clear()
-            else:
-                sense.clear()
-                
-            humidity = sense.get_humidity()
-            return_string += (",humid=%0.0f" % humidity)
+##                sense.show_message("SPA!", text_colour=yellow, back_colour=red)                
+##                time.sleep(1)
+##                sense.clear()
+##                return_string += (","+host_ip)
+#                print(return_string)
 
-            tempc = sense.get_temperature()
-            tempf = c2f(tempc)
-            return_string += (",tempe=%0.0f" % tempf)
+##            else:
+##                sense.clear()
+##                
+##            humidity = sense.get_humidity()
+##            return_string += (",humid=%0.0f" % humidity)
+##
+##            tempc = sense.get_temperature()
+##            tempf = c2f(tempc)
+##            return_string += (",tempe=%0.0f" % tempf)
+##
+##            pressure = sense.get_pressure()
+##            return_string += (",press=%0.0f" % pressure)
+##
+##            orientation = sense.get_orientation_degrees()
+##            return_string += (",pitch={pitch},roll={roll},yaw={yaw}".format(**orientation))
 
-            pressure = sense.get_pressure()
-            return_string += (",press=%0.0f" % pressure)
-
-            orientation = sense.get_orientation_degrees()
-            return_string += (",pitch={pitch},roll={roll},yaw={yaw}".format(**orientation))
-
-            print(return_string)
             UDP_client.sendMessage(return_string)
-
-            time.sleep(1)
+            time.sleep(5)
 
     #############################
     # End main while loop
@@ -123,9 +140,9 @@ if __name__ == '__main__':  #test code
 
     except KeyboardInterrupt:
         print 'Keyboard Interrupt Exception!'
-        sense.clear()
+#        sense.clear()
 
     except IOError:
         print 'I/O Error Exception! Quitting'
-        sense.clear()
+#        sense.clear()
 
---
+++ sense_hat_client.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sleep 10
+cd /home/pi/projects/sense_hat_client
+sudo python sense_hat_client.py
